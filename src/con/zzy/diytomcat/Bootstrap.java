4d8e1eb6b4a57cf53d8a3f6b896c287f52d08807
Zhang Zhiyong <clz14149@ict.nitech.ac.jp>
2021-09-26 16:53:17 +0900

    log file

--- src/con/zzy/diytomcat/Bootstrap.java
+++ src/con/zzy/diytomcat/Bootstrap.java
@@ -4,6 +4,7 @@ import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.util.ArrayUtil;
 import cn.hutool.core.util.NetUtil;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.log.LogFactory;
 import cn.hutool.system.SystemUtil;
 import con.zzy.diytomcat.http.Request;
 import con.zzy.diytomcat.http.Response;
@@ -14,10 +15,15 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class Bootstrap {
     public static void main(String[] args) {
         try {
+            logJVM();
+
             int port = 18080;
             if (!NetUtil.isUsableLocalPort(port)) {
                 System.out.println("ポートがすでに使用されている。");
@@ -43,17 +49,36 @@ public class Bootstrap {
                 } else {
                     String fileName = StrUtil.removePrefix(uri, "/");
                     File file = FileUtil.file(Constant.rootFolder, fileName);
-                    if(file.exists()){
+                    if (file.exists()) {
                         String fileContent = FileUtil.readUtf8String(file);
                         response.getWriter().println(fileContent);
-                    }else{
+                    } else {
                         response.getWriter().println("File Not Found");
                     }
                 }
                 handle200(socket, response);
             }
         } catch (IOException e) {
+            LogFactory.get().error(e);
             e.printStackTrace();
+        }
+    }
+
+    private static void logJVM() {
+        Map<String, String> infos = new LinkedHashMap<>();
+        infos.put("Server version", "zzy DiyTomcat/1.0.1");
+        infos.put("Server built", "2021-09-26 15:28:13");
+        infos.put("Server number", "1.0.1");
+        infos.put("OS Name\t", SystemUtil.get("os.name"));
+        infos.put("OS Version", SystemUtil.get("os.version"));
+        infos.put("Architecture", SystemUtil.get("os.arc"));
+        infos.put("Java Home", SystemUtil.get("java.home"));
+        infos.put("JVM Version", SystemUtil.get("java.runtime.version"));
+        infos.put("JVM Vendor", SystemUtil.get("java.vm.specification.vendor"));
+
+        Set<String> keys = infos.keySet();
+        for(String key : keys){
+            LogFactory.get().info(key + "\t\t" + infos.get(key));
         }
     }
 
