2d63813e9d0857fa22dd08bf19a5b9894c87b170
Zhang Zhiyong <clz14149@ict.nitech.ac.jp>
2021-10-27 19:29:44 +0900

    common class loader

--- src/com/zzy/diytomcat/Bootstrap.java
+++ src/com/zzy/diytomcat/Bootstrap.java
@@ -1,11 +1,29 @@
 package com.zzy.diytomcat;
 
 import com.zzy.diytomcat.catalina.*;
+import com.zzy.diytomcat.classloader.CommonClassLoader;
+
+import java.lang.reflect.Method;
 
 public class Bootstrap {
 
-    public static void main(String[] args) {
-        Server server = new Server();
-        server.start();
+    public static void main(String[] args) throws Exception{
+        CommonClassLoader commonClassLoader = new CommonClassLoader();
+        Thread.currentThread().setContextClassLoader(commonClassLoader);
+        String serverClassName = "com.zzy.diytomcat.catalina.Server";
+        Class<?> serverClass = commonClassLoader.loadClass(serverClassName);
+        Object serverObject = serverClass.newInstance();
+        Method m = serverClass.getMethod("start");
+        m.invoke(serverObject);
+        System.out.println(serverClass.getClassLoader());
+        /**
+         * Bootstrap Class Loader
+         * Extension Class Loader
+         * Application Class Loader
+         * ここからはTomcatのクラスローダー
+         * (Catalina Class Loader　%tomcat_home%/catalina/)  Common Class Loader　%tomcat_home%/lib中のクラスとjarをロード  (Share Class Loader　%tomcat_home%/share)　
+         * Webapp Class Loader 　Webアプリをロードするため，app/WEB-INF/classes,/WEB_INF/lib
+         * Jsp Class Loader
+         */
     }
 }
