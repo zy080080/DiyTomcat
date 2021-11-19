d9ecdf59ec2931172055ca2637d60f88607976fc
Zhang Zhiyong <clz14149@ict.nitech.ac.jp>
2021-11-19 19:52:43 +0900

    post method browser test

--- project/javaweb/src/HelloServlet.java
+++ project/javaweb/src/HelloServlet.java
@@ -21,7 +21,7 @@ public class HelloServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try{
-            System.out.println(this + " doGet()");
+//            System.out.println(this + " doGet()");
             resp.getWriter().println("Hello DIY Tomcat from HelloServlet@javaweb" + this);
 
         }catch(IOException e){
--- project/javaweb/web/hello.jsp
+++ project/javaweb/web/hello.jsp
@@ -1,1 +1,2 @@
-Hello DIY Tomcat from HelloServlet@javaweb, the name is <%=request.getAttribute("name")%>
+Hello DIY Tomcat from HelloServlet@javaweb, the name is <%=request.getAttribute("name")%> </br>
+parameter name =  <%=request.getParameter("name")%>
